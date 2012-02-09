a2e5c8f30b9a402975c90f58355cd2f9dae48242
Benton <bentonrochester@gmail.com>
2012-02-09 13:04:28 -0700

    update ruby blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BdwNotes::Application.config.secret_token = '280c14c1b404a7545d251e0ba55f8854324d22dec4d08ab758d3a56c64682f2314f7d2e29aba559f26ad0fd528726976e483f0fe13e7c743f59f4c237278cef1'
